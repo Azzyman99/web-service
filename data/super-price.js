53ec6d4c150cd72774ee5ad5b3ca553943195adf
Osvaldo Vasquez <91627718+Azzyman99@users.noreply.github.com>
2022-03-24 15:23:34 +0000

    added images and data, edited api

---
+++ data/super-price.js
@@ -0,0 +1,42 @@
+"title": "Super Cars by Price",
+	
+	"Super cars":[
+             {
+				"Manufature":"Mclaren",
+				"Model":"F1",
+				"Year":1992,
+				"0-60":"3.2 Seconds",
+				"Cost Today":"$20,000,000",
+				"Horse Power":"627hp",
+				"Image":"McF1.jpg"
+			  },
+ {
+				"Manufature":"Ford",
+				"Model":"GT",
+				"Year":1966,
+				"0-60":"4.7 Seconds",
+				"Cost Today":"$12,000,000",
+				"Horse Power":"485hp",
+				"Image":"GT.jpg"
+			  },
+    
+{
+				"Manufature":"Ferrari",
+				"Model":"Enzo",
+				"Year":2003,
+				"0-60":"3.1 Seconds",
+				"Cost Today":"$3,800,000",
+				"Horse Power":"660hp",
+				"Image":"enzo.jpg"
+			  },
+     {
+				"Manufature":"Porsche",
+				"Model":"Carrera GT",
+				"Year":2004,
+				"0-60":"3.5 Seconds",
+				"Cost Today":"$1,500,000",
+				"Horse Power":"605hp",
+				"Image":"PcGT.jpg"
+			  },
+  ]
+}
